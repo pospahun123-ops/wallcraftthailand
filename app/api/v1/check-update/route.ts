0c3f3ba958809971373a73cbec6cce8ad87b75d2
twanty979-hue <twanty979@gmail.com>
2026-06-15 09:51:14 +0700

    api new

--- app/api/v1/check-update/route.ts
+++ app/api/v1/check-update/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   try {
     const updateData = {
-      latest_version: "1.0.6",
+      latest_version: "1.0.5",
       // แยก Link ของ Android (APK) และ iOS (TestFlight)
       download_url_android: "https://app.wallcraftthailand.com/base.apk", 
       download_url_ios: "https://testflight.apple.com/join/YOUR_TESTFLIGHT_ID", 
